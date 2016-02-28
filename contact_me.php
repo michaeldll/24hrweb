5c02052aedb3bdf8353135af684c4e7c4a90c0e2
Michael De Laborde <ac2031@hotmail.com>
2016-02-28 12:05:32 +0100

    final

--- contact_me.php
+++ contact_me.php
@@ -16,12 +16,11 @@ $email_address = $_POST['email'];
 $phone = $_POST['phone'];
 $message = $_POST['message'];
 	
-// Create the email and send the message
-$to = 'equipe7.24hweb@gmail.com';
+
+$to = 'ac2031@hotmail.com';
 $email_subject = "Website Contact Form:  $name";
 $email_body = "T'a reçu un message du contact form du site 24H Web. \n\n"."Details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
-$headers = "From: teh.ninja@live.com.pt\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
-$headers .= "Reply-To: $email_address";
+$headers = "From: teh.ninja@live.com.pt\n";
 mail($to,$email_subject,$email_body,$headers);
 return true;			
 ?>
